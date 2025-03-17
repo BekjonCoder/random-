8c1d921bca037be82784c525fc265df73198d029
BekjonCoder <k75154256@gmail.com>
2025-03-17 15:03:52 +0500

    fgfj

--- script.js
+++ script.js
@@ -1,9 +1,27 @@
 'use strict'
 const API='https://jsonplaceholder.typicode.com/users'
-const container=document.querySelector('.container')
-const fetchUser=(resource)=>{
-    const data=fetch(resource)
-    console.log(data);
-    
-}
-fetchUser(API)
+const container=document.querySelector('.card')
+fetch(API)
+.then((data)=>{
+    return data.json()
+}).then((data)=>{
+    data.forEach((user) => {
+        console.log(data);
+        const card=document.createElement('div')
+        card.classList.add('card')
+        card.innerHTML=`
+        <div class="profile-card">
+                <img src="./df.png" alt="">
+                <h2>${user.name}</h2>
+                <p class="job-title">${user.email}</p>
+                <a target href='https:  ' class="website-link">Frontend Developer</a>
+                <p class="location">🚏 San Francisco, Usa</p>
+                <div class="buttons">
+                    <button class="edit-btn">Edit</button>
+                    <button class="follow-btn">Follow</button>
+                </div>
+            </div>
+        `
+        container.appendChild(card)
+    });
+})
